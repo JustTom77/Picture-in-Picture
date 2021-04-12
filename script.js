35f352a6f8b458e182ee9f15a2f4137bf732d8b4
JustTom77 <20codetom20@gmail.com>
2021-04-12 19:59:17 +0200

    finished Project

---
+++ script.js
@@ -0,0 +1,26 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// ==> PROMPT TO SELECT MEDIA STREAM; PASS TO VIDEO ELEMENT, THEN PLAY <==
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("Error here:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // ==> DISABLE BUTTON <==
+  button.disabled = true;
+  // ==> START PICTURE IN PICTURE <==
+  await videoElement.requestPictureInPicture();
+  // ==> RESET BUTTON <==
+  button.disabled = false;
+});
+// ==> ON LOAD <==
+selectMediaStream();
